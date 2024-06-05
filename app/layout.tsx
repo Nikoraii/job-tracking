d2988e9fe6867b068d7dacced146814401af8d56
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-06-06 01:49:59 +0200

    Add background gradient to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
         <body className={inter.className}>
             <StickyNavbar />
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
             {children}
         </body>
     </html>
