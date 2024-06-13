28d753c3885f9384842714f7afbfd9cc4c7cb821
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-06-13 18:31:51 +0200

    Add background gradient

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="flex flex-col justify-around align-middle md:flex-row">
         <div className="flex flex-col mt-4 md:mt-0 gap-4 self-center text-center w-96 md:w-1/2">
           <Typography variant="h1" color="indigo" textGradient>
