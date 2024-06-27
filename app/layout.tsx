778593ef54bd40bb09213c2c6641ad54156930a8
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-06-27 21:48:35 +0200

    Add footer and nextjs-toploader to the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { StickyNavbar } from "./components/Navbar";
+import NextTopLoader from "nextjs-toploader";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body className={inter.className}>
-            <StickyNavbar />
-            <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-            {children}
-        </body>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <body className={inter.className}>
+        <StickyNavbar />
+        <NextTopLoader color="#4f46e5" showSpinner={false} />
+
+        {/* BACKGROUND GRADIENT */}
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
+        <div className="min-h-screen">{children}</div>
+        <Footer />
+      </body>
     </html>
   );
 }
