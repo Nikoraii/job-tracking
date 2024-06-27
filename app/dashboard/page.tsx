4897472489d10ab7e2f3b38ceb388abe0843becc
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-06-27 21:56:13 +0200

    Combine all new components in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,31 @@
-export default function Dashboard() {
-    return (
-        <div>DASHBOARD PAGE</div>
-    )
+"use server";
+
+import { redirect } from "next/navigation";
+import { getUser } from "../auth/actions";
+import { getJobs } from "./actions";
+import FormComponent from "./FormComponent";
+import JobsComponent from "./JobsComponent";
+import GreetingComponent from "./GreetingComponent";
+
+export default async function Dashboard() {
+  const jobs: JobListing[] | null = await getJobs();
+  const user = await getUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="px-4 py-2">
+      {/* Pass user email to component */}
+      <GreetingComponent
+        email={user.data.user?.email ? user.data.user?.email : "user"}
+      />
+
+      {/* Add job form */}
+      <FormComponent />
+
+      <JobsComponent jobsData={jobs} />
+    </div>
+  );
 }
